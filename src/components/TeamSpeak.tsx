4b19ad41173aa1514786dd34fa398f482d32ba67
Janis <janis.meister87@gmail.com>
2023-10-14 19:40:12 +0200

    add open in teamspeak button

--- src/components/TeamSpeak.tsx
+++ src/components/TeamSpeak.tsx
@@ -5,10 +5,19 @@ export default function TeamSpeak() {
 	return (
 		<div className="teamspeak">
 			<h1 id="teamspeak">TeamSpeak</h1>
-			<p className="text">
-				<span>TealFire</span> is a <span>community</span> based around&nbsp;
-				<span>TeamSpeak</span> since <span>2016</span>.
-			</p>
+			<div className="text">
+				<p>
+					<span>TealFire</span> is a <span>community</span> based around&nbsp;
+					<span>TeamSpeak</span> since <span>2016</span>.
+				</p>
+				<button
+					onClick={() => {
+						window.open("ts3server://tealfire.de", "_self");
+					}}
+				>
+					Open In TeamSpeak
+				</button>
+			</div>
 			<TsViewer />
 		</div>
 	);
