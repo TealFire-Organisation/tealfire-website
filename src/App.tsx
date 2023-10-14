a01506be9efb1109723a1a752d584129df8b4f72
Janis <janis.meister87@gmail.com>
2023-10-14 19:02:55 +0200

    fix anchor

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
 import "@styles/App.scss";
 import About from "@components/About";
 import TeamSpeak from "./components/TeamSpeak";
+import { useEffect } from "react";
 
 export default function App() {
+	useEffect(() => {
+		// Auto scroll on achor in url
+		const anchor = window.location.hash;
+		if (anchor) {
+			const element = document.querySelector(anchor);
+			if (element) {
+				element.scrollIntoView({ behavior: "smooth" });
+			}
+		}
+	}, []);
+
 	return (
 		<div className="app">
 			<About />
